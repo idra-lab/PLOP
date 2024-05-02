838e9cdce1883e769d4cc4ae793103b3b55bf262
icosac <enrico.saccon@unitn.it>
2024-05-02 15:48:36 +0200

    Updated Python requirements

--- prolog_planner/examples/building/mappings.pl
+++ prolog_planner/examples/building/mappings.pl
@@ -1,7 +1,8 @@
 mapping(build_pillar_start(A, Pos, Block),
   [
     move_block_start(A, Block, From, Pos),
-    move_block_end(A, Block, From, Pos)]
+    move_block_end(A, Block, From, Pos)
+  ]
 ).
 mapping(place_arch_start(A, Pos1, Pos2, Arch),
   [
